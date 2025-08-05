0c7c26a3392b3c735b8a6aae00b8d72248a3fc56
MatheusBusarello <matheus.b.almeida@outlook.com>
2025-08-05 20:58:29 -0300

    chore: add drizzle setup with initial schema; add seed script

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -14,12 +14,14 @@ export const categoryTable = pgTable("category", {
 });
 
 export const categoryRelations = relations(categoryTable, ({ many }) => ({
-  products: many(productTable)
+  products: many(productTable),
 }));
 
 export const productTable = pgTable("product", {
   id: uuid().primaryKey().defaultRandom(),
-  categoryId: uuid("category_id").notNull().references(() => categoryTable.id),
+  categoryId: uuid("category_id")
+    .notNull()
+    .references(() => categoryTable.id),
   name: text().notNull(),
   slug: text().notNull().unique(),
   description: text().notNull(),
@@ -28,27 +30,31 @@ export const productTable = pgTable("product", {
 
 export const productRelations = relations(productTable, ({ one, many }) => ({
   category: one(categoryTable, {
-      fields: [productTable.categoryId],
-      references: [categoryTable.id],
-    }),
-    variants: many(productVariantTable),
-  }));
+    fields: [productTable.categoryId],
+    references: [categoryTable.id],
+  }),
+  variants: many(productVariantTable),
+}));
 
 export const productVariantTable = pgTable("product_variant", {
   id: uuid().primaryKey().defaultRandom(),
-  productId: uuid("product_id").notNull().references(() => productTable.id),
+  productId: uuid("product_id")
+    .notNull()
+    .references(() => productTable.id),
   name: text().notNull(),
   slug: text().notNull().unique(),
   color: text().notNull(),
-  description: text().notNull(),
   priceInCents: integer("price_in_cents").notNull(),
   imageUrl: text("image_url").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
 });
 
-export const productVariantRelations = relations(productVariantTable, ({ one }) => ({
-  category: one(productTable, {
+export const productVariantRelations = relations(
+  productVariantTable,
+  ({ one }) => ({
+    product: one(productTable, {
       fields: [productVariantTable.productId],
       references: [productTable.id],
     }),
-  }));
+  }),
+);
